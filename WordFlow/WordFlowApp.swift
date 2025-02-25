fcdb11e5c9ba5433673fd82d21ed1e7ef9035a09
LadikBeg <vladbehim@gmail.com>
2025-02-25 20:26:22 +0200

    Initial Commit

---
+++ WordFlow/WordFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordFlowApp.swift
+//  WordFlow
+//
+//  Created by Vladyslav Behim on 25.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct WordFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
