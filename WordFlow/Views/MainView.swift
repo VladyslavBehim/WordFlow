eef1385ef0dfff7d8c9348a9e27eda45a79a0bdc
LadikBeg <vladbehim@gmail.com>
2025-02-25 20:44:33 +0200

    test

---
+++ WordFlow/Views/MainView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  WordFlow
+//
+//  Created by Vladyslav Behim on 25.02.2025.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("test")
+        }
+        
+        .padding()
+    }
+    
+}
+
+#Preview {
+    MainView()
+}
