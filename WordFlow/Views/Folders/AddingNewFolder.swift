f692380db5023da0404ec0fc4a412a0cf4329a81
VladyslavBehim <vladyslavBehim@gmail.com>
2025-03-01 19:20:24 +0200

    Tags for cards | Disign

---
+++ WordFlow/Views/Folders/AddingNewFolder.swift
@@ -0,0 +1,18 @@
+//
+//  AddingNewFolder.swift
+//  WordFlow
+//
+//  Created by Vladyslav Behim on 01.03.2025.
+//
+
+import SwiftUI
+
+struct AddingNewFolder: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddingNewFolder()
+}
---
+++ WordFlow/Views/WorkCard/AddingNewCard.swift
@@ -0,0 +1,18 @@
+//
+//  AddingNewCard.swift
+//  WordFlow
+//
+//  Created by Vladyslav Behim on 01.03.2025.
+//
+
+import SwiftUI
+
+struct AddingNewCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddingNewCard()
+}
